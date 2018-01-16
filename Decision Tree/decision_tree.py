e1fff6a403e943cf1fb943195bc332a262d8ec8a
Kritsana <kritsana.chai@ku.th>
2018-01-16 22:38:38 +0700

    more general split medthod for all attributes

--- Decision Tree/decision_tree.py
+++ Decision Tree/decision_tree.py
@@ -1,24 +1,6 @@
 import csv
 import math
 
-class Attribute(object):
-    def __init__(self, fields):
-        self.name = fields[0]
-        self.fields = fields
-        self.values = set(fields[1:])
-        self.values_count = dict(
-                (v,fields.count(v)) for v in self.values)
-
-class Node(object):
-    def __init__(self, attribute):
-        self.attribute = attribute
-        self.links = dict(
-                (attr, None) for attr in self.attribute.values)
-
-class Tree(object):
-    def __init__(self, root):
-        self.root = root
-
 def entropy(sample):
     summation = sum(sample)
     entropy = 0
@@ -40,7 +22,7 @@ def average_entropy(sample, attribute):
     return weighted_entropy
 
 
-def split(data, attribute):
+def split(data, attribute, attribute_number):
     for i in range(1, len(data)):
         if data[i][5] == 'Yes':
             index = 0
@@ -48,30 +30,29 @@ def split(data, attribute):
             index = 1
 
         for value in attribute.keys():
-            if data[i][1] == value:     # 1 is outlook attribute
+            if data[i][attribute_number] == value:     # 1 is outlook attribute
                 attribute[value][index] += 1
 
     return attribute
 
 data = []
 data_transpose = []
-attributes = {}
+attributes = []
 
 with open('training_example.csv', newline='') as training_example:
     reader = csv.reader(training_example)
     data = [r for r in reader]
     data_transpose = zip(*data)
     data_transpose = [list(x) for x in data_transpose]
-    for row in data_transpose:
-        attributes[row[0]] = Attribute(row)
 
-s = [attributes['PlayTennis'].fields.count('Yes'),
-        attributes['PlayTennis'].fields.count('No')]
+s = [data_transpose[5].count('Yes'),
+        data_transpose[5].count('No')]
 
 outlook = {}
-for value in set(data_transpose[1][1:]):      # 1 is outlook attribute
-    outlook[value] = [0, 0]
+for number in range(1, len(data[0])-1):
+    attributes.append({})
+    for value in set(data_transpose[number][1:]):      # 1 is outlook attribute
+        attributes[number-1][value] = [0, 0]
 
-outlook = split(data, outlook)
-
-print(average_entropy(s,outlook))
+    attributes[number-1] = split(data, attributes[number-1], number)
+    print(average_entropy(s,attributes[number-1]))
