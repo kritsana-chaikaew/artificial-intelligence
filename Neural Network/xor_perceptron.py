63ed1c670a461169f596357b1ec40ed3bce9eb75
Kritsana <kritsana.chai@ku.th>
2018-01-25 10:51:48 +0700

    add back updating

--- Neural Network/xor_perceptron.py
+++ Neural Network/xor_perceptron.py
@@ -2,12 +2,16 @@ import csv
 
 class Node():
     def __init__(self, weight=0):
-        self.inputs = []
-        self.output = 0
         self.initial_weight = weight
         self.weight = initial_weight
-        self.summation = 0
+
+        self.inputs = []
         self.links = []
+        self.back_links = []
+
+        self.summation = 0
+        self.output = 0
+        self.error = 0
 
     def threshold(self, _input):
         if _input > 0:
@@ -25,25 +29,33 @@ class Node():
             link.destination.feed(self.output*link.weight)
 
     def reset(self):        # duplicate call, not efficient
-        self.inputs.clear()
-        self.output = 0
         self.weight = initial_weight
+        self.inputs.clear()
+
+        self.output = 0
         self.summation = 0
+        self.error = 0
 
         for link in self.links:
             link.reset()
 
     def update(self, weight):
-        self.weight += weight
+        self.weight += weight * self.source.output
+
+        for link in back_links:
+            link.update(weight*link.source.output)
 
 
 class Link():
-    def __init__(self, source, destination, weight=0):
-        self.initial_weight = weight
-        self.weight = initial_weight
+    def __init__(self, source, destination, weight=0, back_update=True):
         self.source = source
         self.destination = destination
+        self.initial_weight = weight
+        self.weight = initial_weight
+        self.back_update = back_update
+
         self.source.links.append(self)
+        self.destination.back_links.append(self)
 
     def reset(self):
         self.weight = self.initial_weight
@@ -51,6 +63,12 @@ class Link():
 
     def update(self, weight):
         self.weight += weight
+
+        if back_update:
+            self.source.update(self.weight)
+
+def error(target_output, output):
+    return target_output - output
 
 
 with open('xor_training_set.csv') as training_file:
@@ -69,38 +87,36 @@ for row in training_data:
 
 initial_weight = 0.1
 
-input_node = []
-input_node.append(Node(1))
-input_node.append(Node(1))
+input_nodes = []
+input_nodes.append(Node(1))
+input_nodes.append(Node(1))
 
-hidden_node = []
-hidden_node.append(Node(initial_weight))
-hidden_node.append(Node(initial_weight))
+hidden_nodes = []
+hidden_nodes.append(Node(initial_weight))
+hidden_nodes.append(Node(initial_weight))
 
-output_node = Node(initial_weight)
+output_nodes = []
+output_nodes.append(Node(initial_weight))
 
 links = []
-links.append(Link(input_node[0], hidden_node[0], initial_weight))
-links.append(Link(input_node[0], hidden_node[1], initial_weight))
+links.append(Link(input_nodes[0], hidden_nodes[0], initial_weight, False))
+links.append(Link(input_nodes[0], hidden_nodes[1], initial_weight, False))
 
-links.append(Link(input_node[1], hidden_node[0], initial_weight))
-links.append(Link(input_node[1], hidden_node[1], initial_weight))
+links.append(Link(input_nodes[1], hidden_nodes[0], initial_weight))
+links.append(Link(input_nodes[1], hidden_nodes[1], initial_weight))
 
-links.append(Link(hidden_node[0], output_node, initial_weight))
-links.append(Link(hidden_node[1], output_node, initial_weight))
+links.append(Link(hidden_nodes[0], output_nodes[0], initial_weight))
+links.append(Link(hidden_nodes[1], output_nodes[0], initial_weight))
 
-error = 0
 iteration = 10
+
 for it in range(iteration):
     for i in range(len(inputs)):
-        input_node[0].feed(inputs[i][0])
-        input_node[1].feed(inputs[i][1])
+        input_nodes[0].feed(inputs[i][0])
+        input_nodes[1].feed(inputs[i][1])
 
-        error = target_outputs[i] - output_node.output
-        delta = learning_rate * error
+        for node in output_nodes:
+            node.error = learning_rate * error(target_outputs[i], node.output)
 
-        for link in links:
-            link.update(delta*link.source.output)
-
-        print("%d ^ %d == %d" % (inputs[i][0], inputs[i][1], output_node.output))
+        print("%d ^ %d == %d" % (inputs[i][0], inputs[i][1], output_nodes[0].output))
     print()
