361c4e95e6fd916bf3a85a668961548b4c91cf99
Kritsana <kritsana.chai@ku.th>
2018-02-08 21:29:06 +0700

    import model, layers

--- Neural Network/Fashion-MNIST.py
+++ Neural Network/Fashion-MNIST.py
@@ -2,7 +2,15 @@ from keras.datasets import fashion_mnist
 import numpy as np
 from keras.utils import to_categorical
 import matplotlib.pyplot as plt
+
 from sklearn.model_selection import train_test_split
+
+import keras
+from keras.models import Sequential, Input, Model
+from keras.layers import Dense, Dropout, Flatten
+from keras.layers import Conv2D, MaxPooling2D
+from keras.layers.normalization import BatchNormalization
+from keras.layers.advanced_activations import LeakyReLU
 
 (train_X, train_Y), (test_X, test_Y) = fashion_mnist.load_data()
 
@@ -24,4 +32,6 @@ test_Y_one_hot = to_categorical(test_Y)
 train_X, valid_X, train_label, valid_label = train_test_split(
         train_X, train_Y_one_hot, test_size=0.2, random_state=13)
 
-print(train_X.shape, valid_X.shape, train_label.shape, valid_label.shape)
+batch_size = 64
+epoches = 20
+num_classes = 10
