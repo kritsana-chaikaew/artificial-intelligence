52bbf5689ef12114105c5f67ff0b5b22b5692eb5
Kritsana CHAIKEAW <kritsana.chai@live.ku.th>
2018-02-09 21:28:58 +0700

    refactor

--- Neural Network/Fashion-MNIST.py
+++ Neural Network/Fashion-MNIST.py
@@ -12,6 +12,8 @@ from keras.layers import Conv2D, MaxPooling2D
 from keras.layers.normalization import BatchNormalization
 from keras.layers.advanced_activations import LeakyReLU
 
+from keras.models import load_model
+
 (train_X, train_Y), (test_X, test_Y) = fashion_mnist.load_data()
 
 classes = np.unique(train_Y)
@@ -33,76 +35,85 @@ train_X, valid_X, train_label, valid_label = train_test_split(
         train_X, train_Y_one_hot, test_size=0.2, random_state=13)
 
 batch_size = 64
-epochs = 20
+epochs = 1
 num_classes = 10
 
-fashion_model = Sequential()
-fashion_model.add(Conv2D(
-        32,
-        kernel_size=(3, 3),
-        activation='linear',
-        input_shape=(28, 28, 1),
-        padding='same'))
-fashion_model.add(LeakyReLU(alpha=0.1))
-fashion_model.add(MaxPooling2D((2, 2), padding='same'))
-fashion_model.add(Dropout(0.25))
+def build():
+    model = Sequential()
+    model.add(Conv2D(
+            32,
+            kernel_size=(3, 3),
+            activation='linear',
+            input_shape=(28, 28, 1),
+            padding='same'))
+    model.add(LeakyReLU(alpha=0.1))
+    model.add(MaxPooling2D((2, 2), padding='same'))
+    model.add(Dropout(0.25))
 
-fashion_model.add(Conv2D(
-        64,
-        kernel_size=(3, 3),
-        activation='linear',
-        padding='same'))
-fashion_model.add(LeakyReLU(alpha=0.1))
-fashion_model.add(MaxPooling2D((2, 2), padding='same'))
-fashion_model.add(Dropout(0.25))
+    model.add(Conv2D(
+            64,
+            kernel_size=(3, 3),
+            activation='linear',
+            padding='same'))
+    model.add(LeakyReLU(alpha=0.1))
+    model.add(MaxPooling2D((2, 2), padding='same'))
+    model.add(Dropout(0.25))
 
-fashion_model.add(Conv2D(
-        128,
-        kernel_size=(3, 3),
-        activation='linear',
-        padding='same'))
-fashion_model.add(LeakyReLU(alpha=0.1))
-fashion_model.add(MaxPooling2D((2, 2), padding='same'))
-fashion_model.add(Dropout(0.4))
+    model.add(Conv2D(
+            128,
+            kernel_size=(3, 3),
+            activation='linear',
+            padding='same'))
+    model.add(LeakyReLU(alpha=0.1))
+    model.add(MaxPooling2D((2, 2), padding='same'))
+    model.add(Dropout(0.4))
 
-fashion_model.add(Flatten())
-fashion_model.add(Dense(128, activation='linear'))
-fashion_model.add(LeakyReLU(alpha=0.1))
-fashion_model.add(Dropout(0.3))
-fashion_model.add(Dense(num_classes, activation='softmax'))
+    model.add(Flatten())
+    model.add(Dense(128, activation='linear'))
+    model.add(LeakyReLU(alpha=0.1))
+    model.add(Dropout(0.3))
+    model.add(Dense(num_classes, activation='softmax'))
 
-fashion_model.summary()
+    model.summary()
 
-fashion_model.compile(
-        loss=keras.losses.categorical_crossentropy,
-        optimizer=keras.optimizers.Adam(),
-        metrics=['accuracy'])
+    model.compile(
+            loss=keras.losses.categorical_crossentropy,
+            optimizer=keras.optimizers.Adam(),
+            metrics=['accuracy'])
 
-fashion_train_dropout = fashion_model.fit(
-        train_X, train_label,
-        batch_size=batch_size,
-        epochs=epochs,
-        verbose=1,
-        validation_data=(valid_X, valid_label))
+    return model
 
-fashion_model.save("fashion_model_dropout.h5py")
+def train(model):
+    history = model.fit(
+            train_X, train_label,
+            batch_size=batch_size,
+            epochs=epochs,
+            verbose=1,
+            validation_data=(valid_X, valid_label))
 
-test_eval = fashion_model.evaluate(test_X, test_Y_one_hot, verbose=0)
-print('Test loss:', test_eval[0])
-print('Test accuracy:', test_eval[1])
+    return history
 
-accuracy = fashion_train_dropout.history['acc']
-val_accuracy = fashion_train_dropout.history['val_acc']
-loss = fashion_train_dropout.history['loss']
-val_loss = fashion_train_dropout.history['val_loss']
-epochs = range(len(accuracy))
-plt.plot(epochs, accuracy, 'bo', label='Training accuracy')
-plt.plot(epochs, val_accuracy, 'b', label='Validation accuracy')
-plt.title('Training and validation accuracy')
-plt.legend()
-plt.figure()
-plt.plot(epochs, loss, 'bo', label='Training loss')
-plt.plot(epochs, val_loss, 'b', label='Validation loss')
-plt.title('Training and validation loss')
-plt.legend()
-plt.show()
+def evaluate(model, history):
+    test_eval = model.evaluate(test_X, test_Y_one_hot, verbose=0)
+    print('Test loss:', test_eval[0])
+    print('Test accuracy:', test_eval[1])
+
+    accuracy = history.history['acc']
+    val_accuracy = history.history['val_acc']
+    loss = history.history['loss']
+    val_loss = history.history['val_loss']
+    epochs = range(len(accuracy))
+    plt.plot(epochs, accuracy, 'bo', label='Training accuracy')
+    plt.plot(epochs, val_accuracy, 'b', label='Validation accuracy')
+    plt.title('Training and validation accuracy')
+    plt.legend()
+    plt.figure()
+    plt.plot(epochs, loss, 'bo', label='Training loss')
+    plt.plot(epochs, val_loss, 'b', label='Validation loss')
+    plt.title('Training and validation loss')
+    plt.legend()
+    plt.show()
+
+fashion_model = build()
+model_history = train(fashion_model)
+evaluate(fashion_model, model_history)
